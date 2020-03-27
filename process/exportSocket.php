76c24a7f881088689920e2ab9146ad34c950b0bd
codinghuang <2812240764@qq.com>
2020-03-27 08:11:20 +0000

    add exportSocket example

---
+++ process/exportSocket.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * @Author: your name
+ * @Date: 2020-02-25 14:53:37
+ * @LastEditTime: 2020-02-26 12:19:27
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /swoole/process/test.php
+ */
+
+use Swoole\Process;
+
+$process = new Process(function (Process $process) {
+    $sock = $process->exportSocket();
+    var_dump($sock);
+});
+
+$pid = $process->start();
+
+$sock = $process->exportSocket();
+var_dump($sock);
